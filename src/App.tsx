097f4398d49125395bfd2b1d3a6796d02ffc371d
minugem <closehaan@gmail.com>
2024-07-07 13:44:20 +1200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='login'>
+        <div>
+          <div className="imgPlaceholder"></div>
+        </div>
+        <h1>Welcome to Routine Me</h1>
+        <h2>Get the most out of your day with Routine Me</h2>
+        <button>Get Started</button>
+      </div>
+    </>
+  )
+}
+
+export default App
